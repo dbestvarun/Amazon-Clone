8c108d6761007a023e50e8080964c78f86f9fd11
Varun Parashar <iamvarunparashar@gmail.com>
2024-03-27 00:38:47 +0530

    added javaScript to checkout page

--- data/cart.js
+++ data/cart.js
@@ -1,21 +1,40 @@
-export const cart = [];
+export let cart = [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+}, {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1
+}];
 
 export function addToCart(productId, quantity) {
     let matchingItem;
-  
+
     cart.forEach((cartItem) => {
-      if (productId === cartItem.productId) {
-        matchingItem = cartItem;
-      }
+        if (productId === cartItem.productId) {
+            matchingItem = cartItem;
+        }
     });
-  
+
     if (matchingItem) {
-      matchingItem.quantity += quantity;
+        matchingItem.quantity += quantity;
     }
     else {
-      cart.push({
-        productId,
-        quantity
-      });
+        cart.push({
+            productId,
+            quantity
+        });
     }
-  }
+}
+
+export function removeFromCart(productId) {
+    let newCart = [];
+
+    cart.forEach((cartItem) => {
+        if (cartItem.productId !== productId){
+            newCart.push(cartItem);
+        }
+    });
+
+    cart = newCart;
+
+}
